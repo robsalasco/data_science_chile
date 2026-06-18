953d32c221b85ec2cd5e008178594c38d9642ca1
Roberto Salas <wala.rsalas@gmail.com>
2026-06-18 09:16:05 -0400

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import type { Course } from './types';
 
 export const getData = async (): Promise<Course[]> => {
 	try {
-		return await csv<Course>('/data/datall.csv');
+		return await csv<Course>(`${import.meta.env.BASE_URL}data/datall.csv`);
 	} catch (error) {
 		console.error('Error loading CSV:', error);
 		return [];
